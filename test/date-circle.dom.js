47e3b72655b69244617b4cc937cca4fe0a71f088
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-09-01 17:04:37 +0200

    added more unit tests for DateCircle when combined with small data sets (n=1 or n=3; issue#62)

--- test/date-circle.dom.js
+++ test/date-circle.dom.js
@@ -4,7 +4,7 @@
 
 /*global describe, beforeEach, fixture, crossfilter, afterEach, it, expect, punchcards*/
 
-describe('punchcards DateCircle class...', function () {
+describe('punchcards DateCircle class with cityofchicago-police-data.fixture.json...', function () {
 
     'use strict';
 
@@ -102,5 +102,201 @@ describe('punchcards DateCircle class...', function () {
 
 
 });
+
+
+
+
+describe('punchcards DateCircle class with test/simple-n3.fixture.json...', function () {
+
+    'use strict';
+
+    var fixtures, cf;
+
+    beforeEach(function () {
+
+        var data;
+
+        // set the base directory for loading of fixtures
+        fixture.setBase('test');
+
+        // load the html and json fixtures
+        fixtures = fixture.load('date-circle.fixture.html',
+            'simple-n3.fixture.json');
+
+        data = fixtures[1];
+        cf = crossfilter(data);
+
+    });
+
+
+
+    afterEach(function () {
+        // clean up the DOM
+        fixture.cleanup();
+        cf = undefined;
+    });
+
+
+
+    it('...html fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = fixtures[0][0].outerHTML;
+        expected = '<div id="punchcard-date-circle"></div>';
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = Object.keys(fixtures[1][0]).sort();
+        expected = ["datestr"];
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have 3 records', function () {
+        var actual, expected;
+        actual = cf.size();
+        expected = 3;
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 2 SVG circle elements of class ' +
+        '"symbol" attached to it', function () {
+            var actual, datecircle, symbols;
+            datecircle = new punchcards.DateCircle(cf, 'punchcard-date-circle');
+            datecircle.defineDimensions();
+            datecircle.drawSvg();
+            datecircle.drawHorizontalAxis();
+            datecircle.drawVerticalAxis();
+            datecircle.drawSymbols();
+            symbols = datecircle.svg.select('g.symbol').selectAll('circle.symbol')[0];
+            actual = symbols.length;
+            expect(actual).toEqual(2);
+        });
+
+
+
+});
+
+
+
+
+
+describe('punchcards DateCircle class with test/simple-n1.fixture.json...', function () {
+
+    'use strict';
+
+    var fixtures, cf;
+
+    beforeEach(function () {
+
+        var data;
+
+        // set the base directory for loading of fixtures
+        fixture.setBase('test');
+
+        // load the html and json fixtures
+        fixtures = fixture.load('date-circle.fixture.html',
+            'simple-n1.fixture.json');
+
+        data = fixtures[1];
+        cf = crossfilter(data);
+
+    });
+
+
+
+    afterEach(function () {
+        // clean up the DOM
+        fixture.cleanup();
+        cf = undefined;
+    });
+
+
+
+    it('...html fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = fixtures[0][0].outerHTML;
+        expected = '<div id="punchcard-date-circle"></div>';
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = Object.keys(fixtures[1][0]).sort();
+        expected = ["datestr"];
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have just 1 record', function () {
+        var actual, expected;
+        actual = cf.size();
+        expected = 1;
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 1 SVG circle of class ' +
+        '"symbol" attached to it', function () {
+            var actual, datecircle, symbols;
+            datecircle = new punchcards.DateCircle(cf, 'punchcard-date-circle');
+            datecircle.defineDimensions();
+            datecircle.drawSvg();
+            datecircle.drawHorizontalAxis();
+            datecircle.drawVerticalAxis();
+            datecircle.drawSymbols();
+            symbols = datecircle.svg.select('g.symbol').selectAll('circle.symbol')[0];
+            actual = symbols.length;
+            expect(actual).toEqual(1);
+        });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 1 SVG circle of class ' +
+        '"symbol" attached to it, whose radius should be a positive number', function () {
+
+            // matches the following
+            //
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="0" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="120" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="0" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="120.43" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r=".4" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r=".48982" fill="rgb(0,127,255)"></circle>
+            //
+            // but not
+            //
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="NaN" fill="rgb(0,127,255)"></circle>
+
+            var actual, datecircle, symbols, re;
+            datecircle = new punchcards.DateCircle(cf, 'punchcard-date-circle');
+            datecircle.defineDimensions();
+            datecircle.drawSvg();
+            datecircle.drawHorizontalAxis();
+            datecircle.drawVerticalAxis();
+            datecircle.drawSymbols();
+            symbols = datecircle.svg.select('g.symbol').selectAll('circle.symbol')[0];
+            re = new RegExp('^<circle.*r="(\d{1,}|\d{0,}\.\d{1,})".*><\/circle>$');
+            actual = re.test(symbols[0]);
+            expect(actual).toEqual(true);
+        });
+
+
+
+});
+
 
 
