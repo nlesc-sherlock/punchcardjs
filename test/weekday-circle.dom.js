8e9aa19cd94e2ec24c3b5692740f34fe3e33a62e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-09-01 16:55:37 +0200

    added unit test and fixtures for WeekdayRect, WeekdayCircle when drawing just one svg rect/circle (issue #62)

--- test/weekday-circle.dom.js
+++ test/weekday-circle.dom.js
@@ -4,7 +4,7 @@
 
 /*global describe, beforeEach, fixture, crossfilter, afterEach, it, expect, punchcards*/
 
-describe('punchcards WeekdayCircle class...', function () {
+describe('punchcards WeekdayCircle class with cityofchicago-police-data.fixture.json...', function () {
 
     'use strict';
 
@@ -99,6 +99,205 @@ describe('punchcards WeekdayCircle class...', function () {
             actual = symbols.length;
             expect(actual).toEqual(139);
         });
+
+
+});
+
+
+
+
+
+
+
+
+describe('punchcards WeekdayCircle class with test/simple-n3.fixture.json...', function () {
+
+    'use strict';
+
+    var fixtures, cf;
+
+    beforeEach(function () {
+
+        var data;
+
+        // set the base directory for loading of fixtures
+        fixture.setBase('test');
+
+        // load the html and json fixtures
+        fixtures = fixture.load('weekday-circle.fixture.html',
+            'simple-n3.fixture.json');
+
+        data = fixtures[1];
+        cf = crossfilter(data);
+
+    });
+
+
+
+    afterEach(function () {
+        // clean up the DOM
+        fixture.cleanup();
+        cf = undefined;
+    });
+
+
+
+    it('...html fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = fixtures[0][0].outerHTML;
+        expected = '<div id="punchcard-weekday-circle"></div>';
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = Object.keys(fixtures[1][0]).sort();
+        expected = ["datestr"];
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have 3 records', function () {
+        var actual, expected;
+        actual = cf.size();
+        expected = 3;
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 2 SVG circle elements of class ' +
+        '"symbol" attached to it', function () {
+            var actual, weekdaycircle, symbols;
+            weekdaycircle = new punchcards.WeekdayCircle(cf, 'punchcard-weekday-circle');
+            weekdaycircle.defineDimensions();
+            weekdaycircle.drawSvg();
+            weekdaycircle.drawHorizontalAxis();
+            weekdaycircle.drawVerticalAxis();
+            weekdaycircle.drawSymbols();
+            symbols = weekdaycircle.svg.select('g.symbol').selectAll('circle.symbol')[0];
+            actual = symbols.length;
+            expect(actual).toEqual(2);
+        });
+
+
+
+});
+
+
+
+
+
+describe('punchcards WeekdayCircle class with test/simple-n1.fixture.json...', function () {
+
+    'use strict';
+
+    var fixtures, cf;
+
+    beforeEach(function () {
+
+        var data;
+
+        // set the base directory for loading of fixtures
+        fixture.setBase('test');
+
+        // load the html and json fixtures
+        fixtures = fixture.load('weekday-circle.fixture.html',
+            'simple-n1.fixture.json');
+
+        data = fixtures[1];
+        cf = crossfilter(data);
+
+    });
+
+
+
+    afterEach(function () {
+        // clean up the DOM
+        fixture.cleanup();
+        cf = undefined;
+    });
+
+
+
+    it('...html fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = fixtures[0][0].outerHTML;
+        expected = '<div id="punchcard-weekday-circle"></div>';
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = Object.keys(fixtures[1][0]).sort();
+        expected = ["datestr"];
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have just 1 record', function () {
+        var actual, expected;
+        actual = cf.size();
+        expected = 1;
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 1 SVG circle of class ' +
+        '"symbol" attached to it', function () {
+            var actual, weekdaycircle, symbols;
+            weekdaycircle = new punchcards.WeekdayCircle(cf, 'punchcard-weekday-circle');
+            weekdaycircle.defineDimensions();
+            weekdaycircle.drawSvg();
+            weekdaycircle.drawHorizontalAxis();
+            weekdaycircle.drawVerticalAxis();
+            weekdaycircle.drawSymbols();
+            symbols = weekdaycircle.svg.select('g.symbol').selectAll('circle.symbol')[0];
+            actual = symbols.length;
+            expect(actual).toEqual(1);
+        });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 1 SVG circle of class ' +
+        '"symbol" attached to it, whose radius should be a positive number', function () {
+
+            // matches the following
+            //
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="0" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="120" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="0" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="120.43" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r=".4" fill="rgb(0,127,255)"></circle>
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r=".48982" fill="rgb(0,127,255)"></circle>
+            //
+            // but not
+            //
+            // <circle class="symbol" cx="104" cy="-125.66666666666667" r="NaN" fill="rgb(0,127,255)"></circle>
+
+            var actual, weekdaycircle, symbols, re;
+            weekdaycircle = new punchcards.WeekdayCircle(cf, 'punchcard-weekday-circle');
+            weekdaycircle.defineDimensions();
+            weekdaycircle.drawSvg();
+            weekdaycircle.drawHorizontalAxis();
+            weekdaycircle.drawVerticalAxis();
+            weekdaycircle.drawSymbols();
+            symbols = weekdaycircle.svg.select('g.symbol').selectAll('circle.symbol')[0];
+            re = new RegExp('^<circle.*r="(\d{1,}|\d{0,}\.\d{1,})".*><\/circle>$');
+            actual = re.test(symbols[0]);
+            expect(actual).toEqual(true);
+        });
+
 
 
 });
--- test/weekday-rect.dom.js
+++ test/weekday-rect.dom.js
@@ -4,7 +4,7 @@
 
 /*global describe, beforeEach, fixture, crossfilter, afterEach, it, expect, punchcards*/
 
-describe('punchcards WeekdayRect class...', function () {
+describe('punchcards WeekdayRect class with test/cityofchicago-police-data.fixture.json...', function () {
 
     'use strict';
 
@@ -117,4 +117,167 @@ describe('punchcards WeekdayRect class...', function () {
 
 });
 
+
+
+
+
+describe('punchcards WeekdayRect class with test/simple-n3.fixture.json...', function () {
+
+    'use strict';
+
+    var fixtures, cf;
+
+    beforeEach(function () {
+
+        var data;
+
+        // set the base directory for loading of fixtures
+        fixture.setBase('test');
+
+        // load the html and json fixtures
+        fixtures = fixture.load('weekday-rect.fixture.html',
+            'simple-n3.fixture.json');
+
+        data = fixtures[1];
+        cf = crossfilter(data);
+
+    });
+
+
+
+    afterEach(function () {
+        // clean up the DOM
+        fixture.cleanup();
+        cf = undefined;
+    });
+
+
+
+    it('...html fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = fixtures[0][0].outerHTML;
+        expected = '<div id="punchcard-weekday-rect"></div>';
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = Object.keys(fixtures[1][0]).sort();
+        expected = ["datestr"];
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have 3 records', function () {
+        var actual, expected;
+        actual = cf.size();
+        expected = 3;
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 2 SVG rects of class ' +
+        '"symbol" attached to it', function () {
+            var actual, weekdayrect, symbols;
+            weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
+            weekdayrect.defineDimensions();
+            weekdayrect.drawSvg();
+            weekdayrect.drawHorizontalAxis();
+            weekdayrect.drawVerticalAxis();
+            weekdayrect.drawSymbols();
+            symbols = weekdayrect.svg.select('g.symbol').selectAll('rect.symbol')[0];
+            actual = symbols.length;
+            expect(actual).toEqual(2);
+        });
+
+
+
+});
+
+
+
+
+
+describe('punchcards WeekdayRect class with test/simple-n1.fixture.json...', function () {
+
+    'use strict';
+
+    var fixtures, cf;
+
+    beforeEach(function () {
+
+        var data;
+
+        // set the base directory for loading of fixtures
+        fixture.setBase('test');
+
+        // load the html and json fixtures
+        fixtures = fixture.load('weekday-rect.fixture.html',
+            'simple-n1.fixture.json');
+
+        data = fixtures[1];
+        cf = crossfilter(data);
+
+    });
+
+
+
+    afterEach(function () {
+        // clean up the DOM
+        fixture.cleanup();
+        cf = undefined;
+    });
+
+
+
+    it('...html fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = fixtures[0][0].outerHTML;
+        expected = '<div id="punchcard-weekday-rect"></div>';
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have loaded correctly', function () {
+        var actual, expected;
+        actual = Object.keys(fixtures[1][0]).sort();
+        expected = ["datestr"];
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...json fixture should have just 1 record', function () {
+        var actual, expected;
+        actual = cf.size();
+        expected = 1;
+        expect(actual).toEqual(expected);
+    });
+
+
+
+    it('...after calling .drawSymbols(), the chart should have an ' +
+        'SVG g element of class "symbol" with 1 SVG rect of class ' +
+        '"symbol" attached to it', function () {
+            var actual, weekdayrect, symbols;
+            weekdayrect = new punchcards.WeekdayRect(cf, 'punchcard-weekday-rect');
+            weekdayrect.defineDimensions();
+            weekdayrect.drawSvg();
+            weekdayrect.drawHorizontalAxis();
+            weekdayrect.drawVerticalAxis();
+            weekdayrect.drawSymbols();
+            symbols = weekdayrect.svg.select('g.symbol').selectAll('rect.symbol')[0];
+            actual = symbols.length;
+            expect(actual).toEqual(1);
+        });
+
+
+
+});
 
